5396c4ea644818025ac5f8aca7cb9457145214b7
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-04-27 21:32:44 -0500

    Pruebas de unidad de JSONReader

--- test/utils/JSONReader.test.js
+++ test/utils/JSONReader.test.js
@@ -7,6 +7,10 @@ describe("Prueba de unidad de para JSONReader", () => {
     });
     test('2) Leyendo un archivo en blanco', () => {
         const explorer = JSONReader.readJsonFile('./test/data/explorers_4.test.json')
-        expect(explorer).toBe('Archivo en blanco');
+        expect(explorer).toBe('Error de lectura, archivo en blanco');
+    });
+    test('3) Archivo inexistente', () => {
+        const explorer = JSONReader.readJsonFile('./test/data/explorers_notfound.test.json')
+        expect(explorer).toBe('No se encontró el archivo');
     });
 })
