571cfe011eda720d6fbc20826f41a09a1dc0713b
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-04-28 20:30:24 -0500

    Ajuste de pruebas ExplorerService con falló

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -3,11 +3,11 @@ const ExplorerService = require("../../lib/services/ExplorerService");
 
 describe("Prueba de unidad de para ExplorerService", () => {
     test("1) Leer la lista completa de Explorers", () => {
-        const explorers = JSONReader.readJsonFile("./data/explorers.json");
+        const explorers = JSONReader.readJsonFile("./test/data/explorers_2.json");
         expect(ExplorerService.filterByMission(explorers,"node").length).toBeGreaterThanOrEqual(0);
     });
     test("2) Obtener explorers que sean parte de Node", () => {
-        const explorers = JSONReader.readJsonFile("./data/explorers.json");
+        const explorers = JSONReader.readJsonFile("./test/data/explorers_3.json");
         expect(ExplorerService.getAmountOfExplorersByMission(explorers,"node")).toBe(10);
     });
     test("3) Obtener explorers que sean parte de Node", () => {
